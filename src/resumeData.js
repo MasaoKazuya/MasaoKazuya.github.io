7555f8f01cfb6849a658b8128263e82367a367e7
MasaoKazuya <masaokazuya415@gmail.com>
2023-12-13 01:23:26 -0800

    my first portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,148 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Masao Kazuya",
+    "role": "IT Solutions & Supporter",
+    "linkedinId":"Your LinkedIn Id",
+    "skype": "live:.cid.f2c1963faeea9d7d",
+    "roleDescription": "",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Experienced and results-driven Senior Project Development Manager with a proven track record in leading high-performing teams and delivering successful projects. Adept at overseeing front-end and blockchain development initiatives, implementing robust ERP solutions, and managing end-to-end project lifecycles. Known for strategic thinking, effective stakeholder communication, and a commitment to continuous improvement.",
+    "address":"Japan",
+    "gmail":"masaokazuya415@gmail.com",
+    "education":[
+      {
+        "UniversityName":"University of Yamaguchi",
+        "specialization":"Bachelor of Computer Science",
+        "MonthOfPassing":"Sep",
+        "YearOfPassing":"2012",
+        "Achievements":""
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Several Defi Communities and DAOs",
+        "specialization":"Blockchain Lead Developer and Manager",
+        "MonthOfLeaving":"Nov",
+        "YearOfLeaving":"2023",
+        "Achievements":"NFT Development, Dapp development on EVMOS, Smart Contract Unit test and CI/CD"
+      },
+      {
+        "CompanyName":"Upwork Freelance",
+        "specialization":"Senior Project Manager",
+        "MonthOfLeaving":"Nov",
+        "YearOfLeaving":"2020",
+        "Achievements":"Dynamic, E-Commerce, Sopify Web development, Mobile App development"
+      },
+      {
+        "CompanyName":"Coursework and project",
+        "specialization":"Full-Stack Developer",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2018",
+        "Achievements":"Project Management and Practice, Enterprise Application Development, Database Management"
+      },
+      {
+        "CompanyName":"Proficom GmbH Internship",
+        "specialization":"Senior Project Development Manager",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2017",
+        "Achievements":"Capacity planning and Architecture Design of AWS Infrastructure"
+      },
+      {
+        "CompanyName":"Liventus",
+        "specialization":"Frontend Developer",
+        "MonthOfLeaving":"Sep",
+        "YearOfLeaving":"2016",
+        "Achievements":"Development of modern, responsive customer experiences"
+      },
+    ],
+    "skillsDescription":"Passionate full-stack engineer with 7+ years of hands-on experience in developing web, mobile apps using a wide range of frontend and backend skills like MERN stack, Laravel, React Native, MySQL and MongoDB. And as a blockchain developer I am Mastered JavaScript and Solidity. Extensive knowledge in smart contracts, web3, Ethers consensus algorithms, and data structures. \n Here are my skills:",
+    "skills":[
+      {
+        "skillname":"HTML/CSS UI/UX",
+        "percentage":"95%"
+      },
+      {
+        "skillname":"React/Next/Vue",
+        "percentage":"93%"
+      },
+      {
+        "skillname":"MySQL/MongoDB",
+        "percentage":"95%"
+      },
+      {
+        "skillname":"Laravel/Node",
+        "percentage":"91%"
+      },
+      {
+        "skillname":"ReactNative",
+        "percentage":"94%"
+      },
+      {
+        "skillname":"Web3.js/Ethers.js",
+        "percentage":"90%"
+      },
+      {
+        "skillname":"Solidity",
+        "percentage":"85%"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Custom Website Design",
+        "description":"Landing Page, UI/UX",
+        "imgurl":"images/portfolio/havenly.com.jpg",
+        "url": "https://havenly.com"
+      },
+      {
+        "name":"MERN Stack Development",
+        "description":"E-commerce,Shopify",  
+        "imgurl":"images/portfolio/bidlock.com.jpg",
+        "url": "https://bidlock.com"
+      },
+      {
+        "name":"Mobile App Development",
+        "description":"Shopify",
+        "imgurl":"images/portfolio/designpublic.com.jpg",
+        "url": "https://designpublic.com"
+      },
+      {
+        "name":"Custom DApp Design & development",
+        "description":"NFT Marketplace",
+        "imgurl":"images/portfolio/rarepizzas.com.jpg",
+        "url": "https://rarepizzas.com"
+        
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Great experience, very professional. Would definitely work with again!",
+        "name":"Craig J. Davis"
+      },
+      {
+        "description":"Very responsive and proactive in taken care of my concerns. I have worked with Masao for years and he has always overdelivered.",
+        "name":"James E. Knapp"
+      },
+      {
+        "description":"Quickly helped my website problem efficiently!",
+        "name":"Robert T. Jackson"
+      }
+    ]
+  }
+  
+  export default resumeData
