a10fda5e0f96e365b13fdcb1ba1a1a3e061d9ff1
MasaoKazuya <masaokazuya415@gmail.com>
2023-12-13 01:16:59 -0800

    Updates

---
+++ precache-manifest.c8f493d3e8472c121b065313c5f2df7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b5bbdfcc13ec7b96a50",
+    "url": "/static/css/main.d1d07048.chunk.css"
+  },
+  {
+    "revision": "7b5bbdfcc13ec7b96a50",
+    "url": "/static/js/main.7b5bbdfc.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c14cd80f0b27e3ca9c47dad121148c01",
+    "url": "/index.html"
+  }
+];
